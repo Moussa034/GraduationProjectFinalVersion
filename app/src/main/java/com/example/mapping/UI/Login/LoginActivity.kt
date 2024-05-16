95f831d74c9bbbfe89752d604b3e7d5e7e16755d
diaahany800 <diaahany800@outlook.com>
2024-05-16 16:09:43 +0300

    logo

---
+++ app/src/main/java/com/example/mapping/UI/Login/LoginActivity.kt
@@ -0,0 +1,61 @@
+package com.example.mapping.UI.Login
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginsignupsql.DatabaseHelper
+import com.example.mapping.UI.Home.HomeActivity
+import com.example.mapping.UI.Signup.SignupActivity
+import com.example.mapping.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private val auth = Firebase.auth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.loginButton.setOnClickListener {
+            validate(binding.loginEmail.text.toString(), binding.loginPassword.text.toString())
+        }
+        binding.signupRedirect.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun validate(
+        email: String,
+        password: String
+    ) {
+        if (email.isEmpty()) {
+            binding.loginEmail.error = "Req"
+        } else if (password.isEmpty()) {
+            binding.loginPassword.error = "Req"
+        } else {
+            login(email, password)
+        }
+
+    }
+
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }.addOnFailureListener {
+            Toast.makeText(this,it.message, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/mapping/UI/Signup/SignupActivity.kt
@@ -0,0 +1,92 @@
+package com.example.mapping.UI.Signup
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginsignupsql.DatabaseHelper
+import com.example.mapping.UI.Home.HomeActivity
+import com.example.mapping.SignupModel
+import com.example.mapping.UI.Login.LoginActivity
+import com.example.mapping.USERS
+import com.example.mapping.databinding.ActivitySignupBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.database.database
+
+class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+    private val auth = Firebase.auth
+    private val ref = Firebase.database.reference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.signupButton.setOnClickListener {
+            validate(
+                binding.signupName.text.toString(),
+                binding.signupEmail.text.toString(),
+                binding.signupUsername.text.toString(),
+                binding.signupPassword.text.toString(),
+                binding.signupRePassword.text.toString()
+            )
+
+        }
+        binding.loginRedirect.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun validate(
+        name: String,
+        email: String,
+        username: String,
+        password: String,
+        confPassword: String
+    ) {
+        if (name.isEmpty()) {
+            binding.signupName.error = "Req"
+        } else if (email.isEmpty()) {
+            binding.signupEmail.error = "Req"
+        } else if (username.isEmpty()) {
+            binding.signupUsername.error = "Req"
+        } else if (password.isEmpty()) {
+            binding.signupPassword.error = "Req"
+        } else if (confPassword.isEmpty()) {
+            binding.signupRePassword.error = "Req"
+        } else if (!password.equals(confPassword)) {
+            binding.signupRePassword.error = "not EQ"
+        } else {
+            Signup(name, email, username, password)
+        }
+
+    }
+
+    private fun Signup(name: String, email: String, username: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                sendDataToDataBase(name, email, username)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to create user", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun sendDataToDataBase(name: String, email: String, username: String) {
+        ref.child(USERS).child(auth.uid.toString())
+            .setValue(SignupModel(name, username, email, auth.uid.toString()))
+            .addOnSuccessListener {
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this,it.message, Toast.LENGTH_SHORT).show()
+            }
+    }
+}
