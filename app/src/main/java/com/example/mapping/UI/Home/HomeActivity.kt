09c1edab16475efdd95b9c40e35de3bfe5a8c70b
diaahany800 <diaahany800@outlook.com>
2024-05-16 17:53:58 +0300

    logo

--- app/src/main/java/com/example/mapping/UI/Home/HomeActivity.kt
+++ app/src/main/java/com/example/mapping/UI/Home/HomeActivity.kt
@@ -8,7 +8,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        //auth.signOut()button sign-out
+        //auth.signOut()button sign-out------------------------
 
     }
 }
